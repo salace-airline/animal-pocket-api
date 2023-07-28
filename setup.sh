24e4a127239ca7a22d5f22938e8be3e84dedc4f7
laure_sterne <laure.sterne@student.adatechschool.fr>
2023-07-28 14:12:16 +0200

    maybe move the place of docker-compoer

--- setup.sh
+++ setup.sh
@@ -3,8 +3,10 @@ sudo yum update -y
 sudo dnf update -y
 sudo yum install -y libxcrypt-compat
 sudo yum install docker -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` \
+    >~/docker-compose
+chmod +x ~/docker-compose
+sudo mv ~/docker-compose /usr/local/bin/docker-compose
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chmod 666 /var/run/docker.sock
