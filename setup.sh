9bef416577bb30ce59567eb44eeb9573c0f8cacb
laure_sterne <laure.sterne@student.adatechschool.fr>
2023-07-28 15:41:15 +0200

    we try this way

--- setup.sh
+++ setup.sh
@@ -3,10 +3,8 @@ sudo yum update -y
 sudo dnf update -y
 sudo yum install -y libxcrypt-compat
 sudo yum install docker -y
-sudo curl -L https://github.com/docker/compose/releases/download/3.8.0/docker-compose-`uname -s`-`uname -m` \
-    >~/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/3.8.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x ~/docker-compose
-sudo mv ~/docker-compose /usr/local/bin/docker-compose
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chmod 666 /var/run/docker.sock
