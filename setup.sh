ca7ef050e64daea9a84cf859522d234aba6e86a0
laure_sterne <laure.sterne@student.adatechschool.fr>
2023-07-28 12:00:22 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 sudo yum install docker -y
 python3-pip install docker-compose
 sudo dnf -y install docker-compose
-chmod +x /usr/local/bin/docker-compose
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chmod 666 /var/run/docker.sock
