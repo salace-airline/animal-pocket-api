f4cabc9a3fbec009189bf2e76a47eb06af6cf359
laure_sterne <laure.sterne@student.adatechschool.fr>
2023-07-28 12:13:36 +0200

    install the missing package libxcrypt-compat

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+sudo yum update -y
+sudo dnf update -y
+sudo yum install -y libxcrypt-compat
 sudo yum install docker -y
-sudo pip install docker-compose
-sudo dnf -y install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chmod 666 /var/run/docker.sock
