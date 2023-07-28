4a779ea2d292a4599a26df2f1883823ff84cf5c8
laure_sterne <laure.sterne@student.adatechschool.fr>
2023-07-28 11:56:12 +0200

    install docker-compose with python3

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo yum install docker -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+python3-pip install docker-compose
+sudo dnf -y install docker-compose
 chmod +x /usr/local/bin/docker-compose
 sudo service docker start
 sudo usermod -a -G docker ec2-user
