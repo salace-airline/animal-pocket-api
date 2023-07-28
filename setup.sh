714f0e4f63f3f449b0d227f834a3edcd4fd9720a
laure_sterne <laure.sterne@student.adatechschool.fr>
2023-07-28 15:49:55 +0200

    we really know how to download docker compose now

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo yum update -y
 sudo dnf update -y
 sudo yum install -y libxcrypt-compat
 sudo yum install docker -y
-sudo curl -L "https://github.com/docker/compose/releases/download/3.8.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x ~/docker-compose
 sudo service docker start
 sudo usermod -a -G docker ec2-user
