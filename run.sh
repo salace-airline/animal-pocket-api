08bade9d3ca9ef1a98c7e26dc06ab063b0374eea
laure_sterne <laure.sterne@student.adatechschool.fr>
2023-08-04 17:37:49 +0200

    we are in the good way for the run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-cd /home/ec2-user/animalpocketresources/api
+
+# get Github variables
+export DB_USER=$SECRET_DB_USER
+export DB_PASSWORD=$SECRET_DB_PASSWORD
+export DB_NAME=$SECRET_DB_NAME
 envsubst < docker-compose.yml.template > docker-compose.yml
+
+# run
+cd /home/ec2-user/animalpocketresources/api
 docker-compose build --no-cache
 docker-compose up -d
