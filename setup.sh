53a5a4a64e2367ef82503a1f35de1911b8a4676b
laure_sterne <laure.sterne@student.adatechschool.fr>
2023-10-03 17:32:37 +0200

    Update setup.sh to lastest version of docker compose

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ sudo yum update -y
 sudo dnf update -y
 sudo yum install -y libxcrypt-compat
 sudo yum install docker -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x ~/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chmod 666 /var/run/docker.sock
