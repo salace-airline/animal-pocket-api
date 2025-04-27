dd062851d2fce1e35d3488b5c2caf8d646173b3b
laure-sterne <laure.sterne@gmail.com>
2025-04-27 14:16:03 +0200

    reorgnization of project structure

--- api/database/database.go
+++ api/database/database.go
@@ -1,12 +1,9 @@
 package database
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
-
-	"github.com/salace-airline/animalpocketapi/models"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -26,11 +23,11 @@ func ConnectDb() {
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT")
+		os.Getenv("DB_PORT"),
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info)
+		Logger: logger.Default.LogMode(logger.Info),
 	})
 
 	if err != nil {
@@ -52,71 +49,4 @@ func Migrate() {
 	DB.Db.AutoMigrate(&models.User{}, &models.Fish{}, &models.Bug{}, &models.SeaCreature{})
 }
 
-func IntegrateResources() {
-	log.Println("integrate resources: fishes, bugs and sea creatures")
-	// Fish table
-	fileFish, err := os.Open("./database/fishes.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer fileFish.Close()
-
-	var fishes []models.Fish
-	decoderFish := json.NewDecoder(fileFish)
-	err = decoderFish.Decode(&fishes)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, fish := range fishes {
-		err := DB.Db.Create(&fish).Error
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	fmt.Println("Fishes were successfully insert into database!")
-
-	// Bug table
-	fileBug, err := os.Open("./database/bugs.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer fileBug.Close()
-
-	var bugs []models.Bug
-	decoderBug := json.NewDecoder(fileBug)
-	err = decoderBug.Decode(&bugs)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, bug := range bugs {
-		err := DB.Db.Create(&bug).Error
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	fmt.Println("Bugs were successfully insert into database!")
-
-	// Sea creature table
-	fileSeaCreature, err := os.Open("./database/seaCreatures.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer fileSeaCreature.Close()
-
-	var seaCreatures []models.SeaCreature
-	decoderSea := json.NewDecoder(fileSeaCreature)
-	err = decoderSea.Decode(&seaCreatures)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, SeaCreature := range seaCreatures {
-		err := DB.Db.Create(&SeaCreature).Error
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	fmt.Println("Sea Creatures were successfully insert into database!")
-}
+// Remove the IntegrateResources function as data integration will now be handled by SQL scripts.
